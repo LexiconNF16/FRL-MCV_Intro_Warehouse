dc9e811b3f1968fec1ef70a9f5692181886a7c99
Adrian Lozano <adrian@xenotype.com>
2017-02-02 16:58:16 +0100

    Add project files.

---
+++ Warehouse/Controllers/WorkController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Warehouse.Controllers
+{
+    public class WorkController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
